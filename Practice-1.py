bced4dae0b94a6bbb20455145d1f3dde226109be
Rene-bool <56275019+Rene-bool@users.noreply.github.com>
2019-10-10 00:38:08 +0400

    Задачи из методички

---
+++ Practice-1.py
@@ -0,0 +1,12 @@
+# 1) Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел и
+# строк и сохраните в переменные, выведите на экран.
+
+x = 1
+y = 0
+print('x + y =', x + y)
+z1 = int(input("Enter a number for z1: "))
+z2 = int(input("Enter a number for z2: "))
+a1 = input("Enter a word for a1: ")
+a2 = input("Enter a word for a2: ")
+print('z1 ^ z2 =', z1 ** z2)
+print(a1, a2)
